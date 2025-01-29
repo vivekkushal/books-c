4b65d20641a7ab48a86366182b898697b486219d
vivekkushal <vivekjss1@gmail.com>
2025-01-30 03:20:08 +0530

    feat: Add ex3, modern-c

--- learn-c-the-hard-way/ex2/ex1.c
+++ learn-c-the-hard-way/ex2/ex1.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     int distance = 100;
 
     // this is also a comment
-    printf("Your are %d miles away.\n", distance);
+    printf("You are %d miles away.\n", distance);
 
     return 0;
 }
---
+++ modern-c/bad.c
@@ -0,0 +1,31 @@
+/* This may look like nonsense, but really is -*- mode: C -*- */
+
+/* The main thing that this program does. */
+void main()
+{
+    // Declarations
+    int i;
+    double A[5] = {
+        9.0,
+        2.9,
+        3.E+25,
+        .00007,
+    };
+
+    // Doing some work
+    for (i = 0; i < 5; ++i) {
+        printf("element %d is %g, \tits square is %g\n",
+            i,
+            A[i],
+            A[i] * A[i]);
+    }
+
+    return 0;
+}
+
+void add()
+{
+    for (int i = 0; i < 5; i++) {
+        printf("Hello");
+    }
+}
